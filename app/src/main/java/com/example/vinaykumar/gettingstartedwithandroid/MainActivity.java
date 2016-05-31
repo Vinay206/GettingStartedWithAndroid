7d882e200063fcf2d0502a46d6b6c71a0d768ee8
Vinay206 <k.vinaykumar51@gmail.com>
2016-06-01 02:07:04 +0530

    Intent

--- app/src/main/java/com/example/vinaykumar/gettingstartedwithandroid/MainActivity.java
+++ app/src/main/java/com/example/vinaykumar/gettingstartedwithandroid/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.vinaykumar.gettingstartedwithandroid;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "This is Extra MSG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
 
 
         Button b1= (Button)findViewById(R.id.button);
+        Button b2= (Button)findViewById(R.id.button2);
+
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
                 Toast toast = Toast.makeText(context, text, duration);
                 toast.show();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                EditText editText = (EditText) findViewById(R.id.editText);
+                String message = editText.getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
             }
         });
 
